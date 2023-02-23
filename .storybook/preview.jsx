4594f5906f94cdd5b189d3b9ea0a543c17345c09
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-02-22 17:46:32 -0700

    feat: install Storybook

---
+++ .storybook/preview.jsx
@@ -0,0 +1,20 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider>
+      <Story />
+    </ChakraProvider>
+  ),
+];
